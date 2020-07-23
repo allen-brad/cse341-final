c4c30240f3c93a54a46ee8bb9e50eaf9719e1db3
Brad R. Allen <bradrallen@gmail.com>
2020-07-23 15:52:53 -0600

    corrected port binding

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = require('./app');
 
 const port = (process.env.port || 5000) ;
 
+app.set('port', (process.env.PORT || 5000));
+
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
